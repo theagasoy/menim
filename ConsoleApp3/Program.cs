3709b7bd27d3638988d76583a4011ff9babcdd6c
Classtime <Classtime@PC_3_506_8.aztu.edu.az>
2026-03-04 13:34:29 +0400

    ses

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,191 @@
+﻿// See https://aka.ms/new-console-template for more information
+//int num1 = 3;
+//int num2 = 4;
+//if (num1 > num2) 
+//{
+//    Console.WriteLine("num1");
+//} 
+//else 
+//{
+//    Console.WriteLine("num2");
+//}
+
+
+//int num1 = 123;
+//if (num1 % 2 == 0)
+//{
+//    Console.WriteLine("bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("bolunmur");
+//}
+
+
+
+//Console.WriteLine("eded:");
+//int eded = int.Parse(Console.ReadLine());
+//if (eded >= 100)
+//{
+//    Console.WriteLine("boyuk");
+//}
+//else
+//{ Console.WriteLine( "kicik"); }
+
+
+
+//Console.WriteLine("yas:");
+//int yas = int.Parse(Console.ReadLine());
+//if (yas >= 18)
+//{
+//    Console.WriteLine("adult");
+//}
+//else
+//{ Console.WriteLine("child"); }
+
+
+
+//Console.WriteLine("eded:");
+//int eded = int.Parse(Console.ReadLine());
+//if (eded > 0)
+//{
+//    Console.WriteLine("musbet");
+//}
+//else
+//{ Console.WriteLine("menfi"); }
+
+
+
+//Console.WriteLine("eded:");
+//int gun = int.Parse(Console.ReadLine());
+//switch (gun)
+//{
+//    case 1:
+//        Console.WriteLine("bazar e");
+//        break;
+//    case 2:
+//        Console.WriteLine(" cersenbe ax");
+//        break;
+//    case 3:
+//        Console.WriteLine("cersenbe");
+//        break;
+//    case 4:
+//        Console.WriteLine("cume");
+//        break;
+//    case 5:
+//        Console.WriteLine("cume ax");
+//        break;
+//    case 6:
+//        Console.WriteLine("senbe");
+//        break;
+//    case 7:
+//        Console.WriteLine("bazar");
+//        break;
+//    default:
+//        Console.WriteLine("yoxdur");
+//        break;
+//}
+
+
+
+//Console.WriteLine("eded daxil ed:");
+//double eded = double.Parse(Console.ReadLine());
+//double kub = Math.Pow(eded, 3);
+//Console.WriteLine(kub);
+
+//Console.WriteLine("eded:");
+//int gun = int.Parse(Console.ReadLine());
+//switch (gun)
+//{
+//    case 1:
+//        Console.WriteLine(" 31");
+//        break;
+//    case 2:
+//        Console.WriteLine(" 28");
+//        break;
+//    case 3:
+//        Console.WriteLine("31");
+//        break;
+//    case 4:
+//        Console.WriteLine("30");
+//        break;
+//    case 5:
+//        Console.WriteLine("31");
+//        break;
+//    case 6:
+//        Console.WriteLine("30");
+//        break;
+//    case 7:
+//        Console.WriteLine("31");
+//        break;
+//    case 8:
+//        Console.WriteLine("31");
+//        break;
+//    case 9:
+//        Console.WriteLine(" 30");
+//        break;
+//    case 10:
+//        Console.WriteLine("31");
+//        break;
+//    case 11:
+//        Console.WriteLine("30");
+//        break;
+//    case 12:
+//        Console.WriteLine("30");
+//        break;
+//}
+
+
+//string[] fruits = { "alma", "armud", "nar", "heyva", "gilas", "alca", "banan" };
+//Console.WriteLine(fruits);
+
+
+//string[] students = { "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10" };
+//Console.WriteLine(students);
+
+//string[] students = { "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10" };
+//for (int i = 0; i < students.Length; i++) 
+//{
+//    Console.WriteLine(students[i]);
+//}
+
+//string[] students = { "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10" };
+//Console.WriteLine(students[4]);
+
+
+
+
+
+//int[] num = { 20, 34, 21, 98, 45, 12, 34, 65, 11, 34, 54, 30 };
+//Console.WriteLine("3-ə bolunen ededler:");
+
+//for (int i = 0; i < num.Length; i++)
+//{
+//    if (num[i] % 3 == 0)
+//    {
+//        Console.WriteLine(num[i]);
+//    }
+//}
+
+
+//string[] travels = { "baki", "gence", "seki", "susa" };
+//Console.WriteLine(travels);
+
+
+
+
+//int[] num = { 20, 34, 21, 98, 45, 12, 34, 65, 11, 34, 54, 30 };
+//int sum = 0;
+//for (int i = 0; i < num.Length; i++)
+//{
+//    sum += num[i];
+//}
+//double ort = (double)sum / num.Length;
+//Console.WriteLine("ededi orta: " + ort);
+
+//Array.Sort(num);
+//int min = num[0];
+//int max = num[num.Length - 1];
+//Console.WriteLine("Minimal eded: " + min);
+//Console.WriteLine("Maksimal eded: " + max);
+
