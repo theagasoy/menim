6bf4f26d7299aa0ee147476646164782fe2c9896
Classtime <Classtime@PC_3_506_8.aztu.edu.az>
2026-03-05 12:54:32 +0400

    deyisme

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -141,7 +141,7 @@
 
 
 //string[] students = { "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10" };
-//Console.WriteLine(students);
+//Console.WriteLine(fruits);
 
 //string[] students = { "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10" };
 //for (int i = 0; i < students.Length; i++) 
@@ -189,3 +189,157 @@
 //Console.WriteLine("Minimal eded: " + min);
 //Console.WriteLine("Maksimal eded: " + max);
 
+
+
+//Console.Write("eded daxil ed  ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int a = num / 100;
+//int b = (num / 10) % 10;
+//int c = num % 10;
+//int sum = a * a * a + b * b * b + c * c * c;
+//if (sum==num)
+//    Console.WriteLine("duzdur");
+//else
+//    Console.WriteLine("sehvdir");
+
+//1den 500e geder ededler arasinda tek cut ededleri tap sonrla tek ededlerin cemi ile cut ededlerin cemin cixib alinan ededin musbet yada menfi oldugun tap
+
+//int tek = 0;
+//int cut = 0;
+//for (int i = 1; i <= 500; i++)
+//{
+//    if (i % 2 == 0)
+//    { cut += i; }
+//    else
+//    { tek += i; }
+//}
+//int frq = tek - cut;
+//if (frq > 0)
+//{ Console.WriteLine("musbetdir"); }
+//else if (frq < 0)
+//{ Console.WriteLine("menfidir"); }
+//else { Console.WriteLine("sifir"); }
+
+
+//ilk 10 fibanocci ededin yazan kod
+//int n = 10;
+//int[] fibArr = new int[n];
+
+//fibArr[0] = 0;
+//fibArr[1] = 1;
+
+//for (int i = 2; i < n; i++)
+//{
+//    fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+//}
+//// Massivin elementlərini ekrana çap etmək üçün dövr
+//for (int i = 0; i < fibArr.Length; i++)
+//{
+//    Console.WriteLine(fibArr[i]);
+//}
+
+
+//faktoriali tap
+//Console.Write("ededi yaz ");
+//int n = int.ToInt32(Console.ReadLine());
+
+//long fak = 1; 
+//for (int i = 1; i <= n; i++)
+//{
+//    fak = fak * i; 
+//}
+//Console.WriteLine($"{n}! = {fak}");
+
+//yazilan sozu tersine cevirme
+//Console.Write("soz: ");
+//string soz = Console.ReadLine();
+//string zos = "";
+//for (int i = soz.Length - 1; i >= 0; i--)
+//{
+//    zos += soz[i]; 
+//}
+//Console.WriteLine(zos);
+
+//bir eded daxil edin onun prime olub olmadigini yoxlayin
+//Console.Write("Ededi daxil edin: ");
+//double n = int.Parse(Console.ReadLine());
+//double say = 0;
+//for (int i = 1; i <= n; i++)
+//{
+//    if (n % i == 0)
+//    {
+//        say++;
+//    }
+//}
+//if (say == 2)
+//{ Console.WriteLine("prime "); break; }
+//else
+//{ Console.WriteLine("prime deyil"); }
+
+//int[] num = { 23, 60, 12, 43, 56, 100, 23, 43, 21, 36, 72 };
+//Console.Write("6-ya bölünən ededler:");
+//for (int i = 0; i < num.Length; i++) 
+//{
+//    if ( num[i] % 6 == 0)
+//    {
+//        Console.WriteLine(num[i]);
+//    }
+//}
+
+
+
+
+
+//Console.Write("Birinci eded: ");
+//double eded1 = Convert.ToDouble(Console.ReadLine());
+//Console.Write("sec (+, -, *, /, %, ^): ");
+//string emeliyyat = Console.ReadLine();
+//double netice = 0;
+
+//if (emeliyyat != "^")
+//{Console.Write("Ikinci eded: ");
+//    double eded2 = Convert.ToDouble(Console.ReadLine());
+//    switch (emeliyyat)
+//        {case "+":
+//            netice = eded1 + eded2;
+//            break;
+//        case "-":
+//            netice = eded1 - eded2;
+//            break;
+//        case "*":
+//            netice = eded1 * eded2;
+//            break;
+//        case "/":
+//            netice = eded2 != 0 ? eded1 / eded2 : 0;
+//            if (eded2 == 0) Console.WriteLine("Sifira bolmek olmaz");
+//            break;
+//        case "%":
+//            netice = eded1 % eded2;
+//            break;
+//        default:
+//            Console.WriteLine("Yanlisdir");
+//            return;}
+//}
+//else
+//{netice = eded1 * eded1;}
+//Console.WriteLine($"Netice: {netice}")
+
+
+//double[] prices = { 100.0, 1300.50, 259.99, 430.30, 160.99, 320.0 };
+//Console.Write("qiymet: ");
+//for (int i = 0; i < prices.Length; i++) 
+//{
+//    double prc = prices[i] * 0.8;
+//    Console.WriteLine($"{prices[i]}");
+//}
+
+
+//iscilerin yasi olan bir array verilib yasi 65e beraber ve boyuk olanlari teqaude ayrilmali oldugunu bildiren mesaj gelsin
+int[] age = { 23, 34, 19, 65, 40, 66, 29, 35, 67, 64, 60, 32, 57 };
+for (int i = 0; i < age.Length; i++)
+{
+    if (age[i] >= 65)
+    {
+        Console.WriteLine($"{age[i]} yaşlı işçi: Təqaüdə ayrılmalıdır.");
+    }
+}
